919c824f6fcdd2c779d3d2a8c585184337c83c48
candraprastyadiwahab <prastyadi92@gmail.com>
2019-02-24 16:56:23 +0700

    first commit

---
+++ src/main/java/latihan/candra/App.java
@@ -0,0 +1,13 @@
+package latihan.candra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Candra Ganteng!" );
+    }
+}
